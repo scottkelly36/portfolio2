61b0910b6dbcf799fa2307e38dbeec93d3ba90ed
scott Kelly <scottkelly36@gmail.com>
2022-05-20 15:30:09 +0100

    updated some mob views

--- dist/script.js
+++ dist/script.js
@@ -4,3 +4,9 @@ const menu = document.querySelector("#dropdown");
 hamburger.addEventListener("click", () => {
   menu.classList.toggle("visible")
 })
+
+const copyRight = document.querySelector('#copyright')
+const date = new Date()
+
+
+copyRight.innerHTML = `Metaa Ⓒ ${date.getFullYear()}`
